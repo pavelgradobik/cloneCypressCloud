e594fda17258b7b628711d7379b06a9404700796
Pavlo Gradobik <gradobik@gmail.com>
2024-01-23 19:03:04 +0200

    add base tests
    add cypress.yml file to run tests in with a github actions and send the resuts to the cloud

--- cypress/e2e/cloudRunCheck.cy.js
+++ cypress/e2e/cloudRunCheck.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Open e-commerce catalogs', () => {
+	const hotlineMainPageUrl = 'https://hotline.ua/';
+	const ekUrl = 'https://ek.ua/ua/';
+
+	it('open Hotline and verify page opened', () => {
+    const inputPlaceholderText = 'Знайти товар, магазин, бренд';
+    const catalogButtonText = 'Каталог';
+    const logoAltText = 'Hotline - порівняти ціни в інтернет-магазинах України'; 
+
+		cy.visit(hotlineMainPageUrl);
+    cy.get('.header-catalog-button').should('be.visible').should('contain', catalogButtonText);
+    cy.get('img').should('be.visible').should('have.attr', 'alt', logoAltText);
+	});
+
+	it('open EK and verify page opened', () => {
+		cy.visit(ekUrl);
+    cy.get('a[title="E-Katalog"]').should('be.visible');
+    cy.get('#ek-search-form').should('be.visible');
+    cy.get('.sp-carousel-viewport').should('be.visible');
+	});
+});
